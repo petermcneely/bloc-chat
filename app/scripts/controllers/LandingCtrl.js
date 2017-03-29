21a9b0ee44dda591cbbc206497a54d766fc53152
Peter McNeely <pete.mcneely@gmail.com>
2017-03-28 21:44:25 -0400

    Fix for currentMessages updating.

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -2,32 +2,21 @@
 	angular
 		.module('blocChat')
         .controller('LandingCtrl', ['Room', 'Message', '$scope', function (Room, Message, $scope) {
-            this.currentMessages = undefined;
-            this.currentRoom = undefined;
+            var self = this;
+            self.currentMessages = undefined;
+            self.currentRoom = undefined;
 
-            this.updateRoom = function (id, value) {
-                this.currentRoom = value;
+            self.updateRoom = function (id, value) {
+                self.currentRoom = value;
                 Message.getByRoomId(id).then(
                     function (success) {
-                        
-                        this.currentMessages = success;
-                        alert(JSON.stringify(this.currentMessages));
-                        //console.log(this.currentMessages);
+                        self.currentMessages = success;
                         $scope.$apply();
                     },
                     function (failure) {
                         console.log(failure);
                     }
                 );
-
-                //Message.getByRoomId(id).then(function (data) {
-                //    this.currentMessages = data;
-                //}).then(function (success) {
-                //    console.log('here?');
-                //    console.log(success);
-                //    }).then(function (success) {
-                //        console.log("here too?");
-                //});
             };
         }]);
 })();
