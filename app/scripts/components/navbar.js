d878d191ee124ebf9099745f08b26cba9a29914f
MIACANALYTICS\pmcneely <pete.mcneely@miacanalytics.com>
2017-04-06 14:47:36 -0400

    Add navbar support for signing out.

---
+++ app/scripts/components/navbar.js
@@ -0,0 +1,38 @@
+﻿(function () {
+
+    var navbar = {
+        templateUrl: '../templates/navbar.html',
+        controller: function () {
+            var self = this;
+            self.user = undefined;
+
+            self.signOut = function () {
+                if (self.user) {
+                    firebase.auth().signOut();
+                    self.user = null;
+                }
+            };
+
+            self.$onInit = function () {
+                firebase.auth().onAuthStateChanged(function (user) {
+                    if (user) {
+                        user.getToken().then(function (accessToken) {
+                            self.user = user;
+                        },
+                        function (error) {
+                            self.user = null;
+                        });
+                    } else {
+                        self.user = null;
+                    }
+                }, function (error) {
+                    console.log(error);
+                });
+            };
+        }
+    };
+
+    angular
+        .module('blocChat')
+        .component('navBar', navbar);
+})();
